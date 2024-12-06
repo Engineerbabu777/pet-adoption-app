f637c96b2a29d54e249b74e30edf599becee2121
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-12-06 22:22:32 +0500

    Add: border created

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -2,11 +2,27 @@
 
 import React from 'react';
 import { View, Text } from 'react-native';
+import Header from '../../components/Home/Header';
 
 export default function Home() {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={{
+      padding:20,marginTop:20,
+    }}>
+    {/* HEADER */}
+    <Header />
+
+    {/* SLIDER! */}
+
+
+    {/* CATEGORY! */}
+
+
+
+    {/* LIST OF PETS! */}
+
+
+    {/* ADD NEW PET OPTIONS! */}
      </View>
   );
 }
